232c0133b6207735b6fa73a5016fc9a4ff2a1c59
Yoshiki Shin'ya <785431ys@gmail.com>
2023-05-23 15:06:13 +0900

    Streamを使った数列の処理

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.List;
+public class Main {
+
+    public static void main(String[] args){
+        List<Integer> numbers = List.of(1,2,3,4,5,6);
+        numbers.stream().forEach(System.out::println);
+        System.out.println("*************************************");
+        numbers.stream().filter(number -> number % 2 != 0).forEach(System.out::println); // 偶数を表示
+        System.out.println("*************************************");
+        numbers.stream().filter(number -> number % 2 == 0).forEach(System.out::println); // 奇数を表示
+    }
+}

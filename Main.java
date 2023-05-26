d35edccbae06afaf187809735bfacbd7b0b7816b
Yoshiki Shin'ya <785431ys@gmail.com>
2023-05-26 12:06:27 +0900

    コンフリクトの解決

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args){
+        // コンフリクトの解決
         List<Integer> numbers = List.of(1,2,3,4,5,6);
         System.out.println("*************************************");
         numbers.stream().filter(number -> number % 2 != 0).forEach(System.out::println); // 偶数を表示
